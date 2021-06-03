f03da543069577626c780c5870dd135282d947ac
RedMadda <maddalenarossaro91@gmail.com>
2021-06-03 16:54:03 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -7,15 +7,25 @@ var numeri = [];
 for (var i = 1; i <= 100; i++){
     numeri.push(i);
 }
-for (i = 0; i < numeri.length; i++){
- 
-  if (numeri[i] % 3 == 0 && numeri[i] % 5 == 0){
-      numeri[i] += " " + "BuzzFizz";
-  } else if (numeri[i] % 3 == 0 ){
-    numeri[i] += " " + "Fizz";
-  } else if (numeri[i] % 5 == 0){
-    numeri[i] += " " + "Buzz";
-  }
 
+for (i = 0; i < numeri.length; i++){
+    if (numeri[i] % 3 == 0 && numeri[i] % 5 == 0 && numeri[i] % 6 == 0){
+        numeri[i] += " " + "FizzBuzzBoolean";
+    } else if (numeri[i] % 3 == 0 && numeri[i] % 5 == 0){
+      numeri[i] += " " + "FizzBuzz";
+    } else if (numeri[i] % 3 == 0 && numeri[i] % 6 == 0){
+        numeri[i] += " " + "FizzBoolean";
+    } else if (numeri[i] % 3 == 0 ){
+    numeri[i] += " " + "Fizz";
+    } else if (numeri[i] % 5 == 0){
+    numeri[i] += " " + "Buzz";
+    }
   document.getElementById("stampa").innerHTML += numeri[i] + "</br>";
 }
+
+console.log(numeri);
+
+
+// Se il numero è anche divisibile per 6, aggiungiamo "Boolean".
+// 6 diventerà quindi FizzBoolean,
+// 30 diventerà FizzBuzzBoolean
